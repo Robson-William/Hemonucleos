679c2c67d4747b1827569ff6833df4411414503a
Robson William <robsonwilliamro@gmail.com>
2022-11-21 09:56:01 -0300

    Adicionando método para sincronização

--- controllers/LocalizacaoController.js
+++ controllers/LocalizacaoController.js
@@ -12,3 +12,10 @@ const addLocal = async (req, res) => {
 		res.status(400).send('Falha ao salvar!');
 	})
 };
+
+const sync = async () =>{
+	await Local.sync();
+	console.log('Banco de dados sincronizado!');
+}
+
+module.exports = {addLocal, sync};
--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 // Dependencias
 require('dotenv').config();
 const express = require('express');
+const LocalizacaoController = require('./controllers/LocalizacaoController');
 
 const PORT = process.env.API_PORT
 const app = express();
 app.use(express.json());
+/*async function  testar() {try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+}*/
+LocalizacaoController.sync();
 
 // Rotas
+app.post('/localizacao', LocalizacaoController.addLocal);
 
+// Porta
 app.listen(PORT, console.log(`A aplicação está rodando na porta ${PORT}`));
+
